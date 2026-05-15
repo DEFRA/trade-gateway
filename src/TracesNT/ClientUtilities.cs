10638d2109605a24dcf6f77830e344fd2f299d5e
Nick Pattman <nick.pattman@equalexperts.com>
2026-05-15 14:01:45 +0100

    CDMS-1408 simplify client disposal

--- src/TracesNT/ClientUtilities.cs
+++ src/TracesNT/ClientUtilities.cs
@@ -6,10 +6,10 @@ namespace TracesNT
     {
         public static async Task CloseClient<T>(T client) where T : class
         {
-            if (client is ClientBase<T> clientBase)
+            if (client is IAsyncDisposable disposableClient)
             {
                 // IAsyncDisposable closes or aborts the client based on state
-                await ((IAsyncDisposable)clientBase).DisposeAsync();
+                await disposableClient.DisposeAsync();
             }
         }
     }
