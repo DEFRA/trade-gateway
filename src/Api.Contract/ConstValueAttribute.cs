fd94612c13d40672b3c9a7f66909f7bff10c69c8
Nick Pattman <nick.pattman@equalexperts.com>
2026-06-11 14:04:37 +0100

    CDMS-1472 removed incorrectly generated schema files

--- src/Api.Contract/ConstValueAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace Trade.Gateway.Api.Contract;
-
-[AttributeUsage(AttributeTargets.Property)]
-public sealed class ConstValueAttribute(string value) : Attribute
-{
-    public string Value { get; } = value;
-}
