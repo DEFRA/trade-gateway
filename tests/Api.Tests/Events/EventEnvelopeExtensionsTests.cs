b9791b0e872b55019db1b1afc1201a40a372c818
Thomas Andoerson <t11omas1983@live.co.uk>
2026-08-03 11:22:40 +0100

    Added tests

---
+++ tests/Api.Tests/Events/EventEnvelopeExtensionsTests.cs
@@ -0,0 +1,36 @@
+using Trade.Gateway.Api.Contract.Certificate;
+using Trade.Gateway.Api.Contract.Events;
+
+namespace Api.Tests.Events
+{
+    public class EventEnvelopeExtensionsTests
+    {
+        [Fact]
+        public async Task ConvertChedToEventTest()
+        {
+            var ched = new DefraUNVTDCHEDProfile()
+            {
+                SpecifiedConsignment = new Consignment(),
+                ExchangedDocument = new ExchangedDocument() { Identifier = "CHEDA-GB-2026-1234567" },
+            };
+
+            var @event = ched.ToEventEnvelope("testCorrelationId");
+
+            await Verify(@event);
+        }
+
+        [Fact]
+        public async Task ConvertIntraToEventTest()
+        {
+            var intra = new DefraUNVTDINTRAProfile()
+            {
+                SpecifiedConsignment = new Consignment(),
+                ExchangedDocument = new ExchangedDocument() { Identifier = "CHEDP-GB-2026-1234567" },
+            };
+
+            var @event = intra.ToEventEnvelope("testCorrelationId");
+
+            await Verify(@event);
+        }
+    }
+}
