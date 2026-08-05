95c96abe8435466e2ac89fe8fd41933ad7867e28
Thomas Andoerson <t11omas1983@live.co.uk>
2026-08-05 10:13:19 +0100

    fixed tests

--- tests/Api.Tests/OpenApi/FindCertificatesRequestTests.cs
+++ tests/Api.Tests/OpenApi/FindCertificatesRequestTests.cs
@@ -72,7 +72,8 @@ public class FindCertificatesRequestTests
     {
         // Arrange
         var request = CreateValidRequest();
-        request.UpdatedFrom = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Local);
+        request.UpdatedFrom = 
+            new DateTimeOffset(new DateTime(2024, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), TimeSpan.FromHours(1));
 
         // Act
         var results = ValidateModel(request);
@@ -91,7 +92,8 @@ public class FindCertificatesRequestTests
     {
         // Arrange
         var request = CreateValidRequest();
-        request.UpdatedBefore = new DateTime(2024, 1, 2, 0, 0, 0, DateTimeKind.Local);
+        request.UpdatedBefore =
+            new DateTimeOffset(new DateTime(2024, 1, 2, 0, 0,0,0, DateTimeKind.Unspecified), TimeSpan.FromHours(1));
 
         // Act
         var results = ValidateModel(request);
